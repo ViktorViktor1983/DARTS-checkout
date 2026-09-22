40ba49e78d35e586d1ed2f8c031af4cf32526902
ViktorViktor1983 <lodkin-viktor2@mail.ru>
2026-09-22 19:56:02 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.dartscheckout"
+        applicationId = "com.dartscheckout.viktor"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
