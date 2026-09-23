b6c8c6a7cf0a531948e5bc3cacd7989ac5523cf8
ViktorViktor1983 <lodkin-viktor2@mail.ru>
2026-09-23 19:58:41 +0300

    Create Colors.kt

---
+++ app/src/main/java/com/example/dartscheckout/theme/Colors.kt
@@ -0,0 +1,19 @@
+package com.example.dartscheckout.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBg = Color(0xFF121212)
+val TileBg = Color(0xFF4A6572)
+val TileBgDark = Color(0xFF37474F)
+val Accent = Color(0xFF4FC3F7)
+val OpColor = Color(0xFF0288D1)
+val GoldAccent = Color(0xFFFFD54F)
+val MainVariantColor = Color(0xFF4FC3F7)
+val AltVariantColor = Color(0xFFFFFFFF)
+val BackupVariantColor = Color(0xFFB39DDB)
+val MissVariantColor = Color(0xFFFFAB91)
+val ImpossibleBg = Color(0xFF5C2B3A)
+val ImpossibleText = Color(0xFFEF9A9A)
+val ErrorColor = Color(0xFFEF5350)
+
+val IMPOSSIBLE_NUMBERS = setOf(159, 162, 163, 165, 166, 168, 169)
